2344396cf823637aa99cd227c29b0719465de855
great-gravity <prav1n.5ridhar02@gmail.com>
2025-04-13 10:04:25 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md", "w+", encoding="utf-8") as f:
     long_description = f.read()
 
 __version__ = "0.0.0"
-REPO_NAME = "Abstraction-Text-Summarization"
-AUTHOR_UERS_NAME = "pravinsridhar"
+REPO_NAME = "Text-Summarizer-App"
+AUTHOR_UERS_NAME = "great-gravity"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "pravinsrdhr@gmail.com"
+AUTHOR_EMAIL = "prav1n.5ridhar02@gmail.com"
 
 setuptools.setup(
     name=SRC_REPO,
@@ -17,9 +17,9 @@ setuptools.setup(
     description="A small python package for NLP app",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"",
+    url=f"https://github.com/{AUTHOR_UERS_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker": f"",
+        "Bug Tracker": f"https://github.com/{AUTHOR_UERS_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"":"src"},
     packages=setuptools.find_packages(where="src")
